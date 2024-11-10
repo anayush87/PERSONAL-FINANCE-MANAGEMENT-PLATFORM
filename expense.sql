68b91c692e900870b95b8d6d0a8d7d1a318bb85e
anayush87 <150177653+anayush87@users.noreply.github.com>
2024-11-10 10:56:08 +0530

    Delete expense.sql

--- expense.sql
+++
@@ -1,9 +0,0 @@
-USE expense_tracker;
-
-CREATE TABLE expenses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    amount DECIMAL(10, 2) NOT NULL,
-    date DATE NOT NULL
-);
